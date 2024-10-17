5c46a50583dd9993818e5a51f4a9d51afef564e0
manaralnaasan <52988882+manaralnaasan@users.noreply.github.com>
2024-10-17 12:06:51 +0900

    losses.py

--- model/losses.py
+++ model/losses.py
@@ -1,1 +1,11 @@
+import torch
+import torch.nn.functional as F
+
+def dice_loss(pred, target):
+    smooth = 1.0
+    intersection = (pred * target).sum()
+    return 1 - (2. * intersection + smooth) / (pred.sum() + target.sum() + smooth)
+
+def smooth_l1_loss(pred_bbox, target_bbox):
+    return F.smooth_l1_loss(pred_bbox, target_bbox)
 
